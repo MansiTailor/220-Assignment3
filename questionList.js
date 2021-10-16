cefe5c453693d05fa2f810bec0029ccab4271439
MansiTailor <92160640+MansiTailor@users.noreply.github.com>
2021-10-16 20:36:04 +0530

    Add files via upload

---
+++ questionList.js
@@ -0,0 +1,44 @@
+const questions = [
+  {
+    question : "Where was Deepika Padukone born?",
+    answer : "Denmark",
+    options: ["India","UK","Denmark","Australia"]
+  },
+  {
+    question : "Deepika Padukone is the daughter of which famous former Indian Badminton player?",
+    answer : "Prakash Padukone",
+    options: ["Prakash Padukone","Prakash jain","Prakash Bhardwaj","Prakash Kohli"]
+  },
+  {
+    question : "Which is Deepika's Hollywood debut film?",
+    answer : "XXX-The Return of Xander Cage",
+    options: ["Fast & Furious 7","The Witch Hunter","The white house down","XXX-The Return of Xander Cage"]
+  },
+  {
+    question : "Deepika has played which sport on a national level?",
+    answer : "Badminton",
+    options: ["Cricket","Badminton","Table Tennis","Kabbadi"]
+  },
+  {
+    question : "Which was Deepika Padukone's Debut Film??",
+    answer : "Om Shanti Om",
+    options: ["Om Shanti Om","Break ke Baad","Janneman","Ashwariya"]
+  },
+  {
+    question : "When did Deepika get married?",
+    answer : "2018",
+    options: ["2012","2015","2018","2019"]
+  },
+  {
+    question : "In which of the following movies Deepika Padukone played the role of the daughter of a constipated father along with Irrfan Khan ?",
+    answer : "Piku",
+    options: ["Padmaavat","DDLJ","Piku","Happy New Year"]
+  },
+  {
+    question : " In which film did Deepika Padukone play the role of Leela ?",
+    answer : "Ram Leela",
+    options: ["Padmaavat","Bajirao Mastani","Piku","Ram Leela"]
+  },
+]
+
+module.exports = questions;
